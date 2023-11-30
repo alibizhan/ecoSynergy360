dd3e958066825dfab7e620c070f2d6b8201ee9dd
Ali Blau <alibizhan.samim@gmail.com>
2023-11-30 17:27:42 +0300

    update ecoSynergy360.py

--- ecoSynergy360.py
+++ ecoSynergy360.py
@@ -1,96 +1,96 @@
-import streamlit as st
+# import streamlit as st
 
-# Title and introductory text for the first page
-st.title('WELCOME TO THE ECOSYNERGY360 PLATFORM ON SUSTAINABILITY ASSESSMENT')
-st.markdown(
-    """
-    The EPSA is developed by Ecosynergy360 team...
-    """
-)
+# # Title and introductory text for the first page
+# st.title('WELCOME TO THE ECOSYNERGY360 PLATFORM ON SUSTAINABILITY ASSESSMENT')
+# st.markdown(
+#     """
+#     The EPSA is developed by Ecosynergy360 team...
+#     """
+# )
 
-st.write("---")
+# st.write("---")
 
-st.title("Select a Section")
+# st.title("Select a Section")
 
-# Define custom CSS for styling the buttons
-button_style = """
-    <style>
-    .button-style {
-        background-color: #f63366;
-        color: white;
-        padding: 10px 20px;
-        border-radius: 5px;
-        border: none;
-        margin-right: 10px;
-        cursor: pointer;
-    }
-    </style>
-"""
+# # Define custom CSS for styling the buttons
+# button_style = """
+#     <style>
+#     .button-style {
+#         background-color: #f63366;
+#         color: white;
+#         padding: 10px 20px;
+#         border-radius: 5px;
+#         border: none;
+#         margin-right: 10px;
+#         cursor: pointer;
+#     }
+#     </style>
+# """
 
-# Display the custom CSS style
-st.markdown(button_style, unsafe_allow_html=True)
+# # Display the custom CSS style
+# st.markdown(button_style, unsafe_allow_html=True)
 
-# Create buttons for different sections in the main page using HTML
-if "<button class='button-style' onclick='Sustainability()'>Sustainability</button>":
-    st.title("Sustainability Context")
-    st.markdown(
-        """
-        Welcome to Ecosynergy360, where sustainability is not just a concept but a commitment to securing the needs of the present generation without compromising the essential requirements of the generations to come.
-        ...
-        """
-    )
-    # Add more content for Sustainability section...
+# # Create buttons for different sections in the main page using HTML
+# if "<button class='button-style' onclick='Sustainability()'>Sustainability</button>":
+#     st.title("Sustainability Context")
+#     st.markdown(
+#         """
+#         Welcome to Ecosynergy360, where sustainability is not just a concept but a commitment to securing the needs of the present generation without compromising the essential requirements of the generations to come.
+#         ...
+#         """
+#     )
+#     # Add more content for Sustainability section...
 
-if "<button class='button-style' onclick='LifeCycleAssessment()'>Life Cycle Assessment</button>":
-    st.title("Life Cycle Assessment")
-    st.markdown(
-        """
-        Welcome to Ecosynergy360, where sustainability isn't just a goal but a journey guided by Life Cycle Assessment (LCA), a powerful tool in understanding the holistic impact of projects on our environment.
-        ...
-        """
-    )
-    # Add more content for LCA section...
+# if "<button class='button-style' onclick='LifeCycleAssessment()'>Life Cycle Assessment</button>":
+#     st.title("Life Cycle Assessment")
+#     st.markdown(
+#         """
+#         Welcome to Ecosynergy360, where sustainability isn't just a goal but a journey guided by Life Cycle Assessment (LCA), a powerful tool in understanding the holistic impact of projects on our environment.
+#         ...
+#         """
+#     )
+#     # Add more content for LCA section...
 
-if "<button class='button-style' onclick='CarbonFootprint()'>Carbon Footprint</button>":
-    st.title("Carbon Footprint")
-    st.markdown(
-        """
-        In the realm of sustainable practices, the concept of a "carbon footprint" has emerged as a crucial metric in assessing the environmental impact of human activities.
-        ...
-        """
-    )
-    # Add more content for Carbon Footprint section...
+# if "<button class='button-style' onclick='CarbonFootprint()'>Carbon Footprint</button>":
+#     st.title("Carbon Footprint")
+#     st.markdown(
+#         """
+#         In the realm of sustainable practices, the concept of a "carbon footprint" has emerged as a crucial metric in assessing the environmental impact of human activities.
+#         ...
+#         """
+#     )
+#     # Add more content for Carbon Footprint section...
 
-if "<button class='button-style' onclick='Calculator()'>Calculator</button>":
-    st.title("Calculator")
-    # Add functionality for the Calculator section...
-
-
-# if st.button("Calculator", help="Explore Calculator", key="calculator_button", class_="button-style"):
+# if "<button class='button-style' onclick='Calculator()'>Calculator</button>":
 #     st.title("Calculator")
-#     phase = st.radio("Choose the phase of your project", ("Production phase", "Construction Phase"))
 #     # Add functionality for the Calculator section...
 
 
-# ##else:  # Calculator section
-#     ##st.title("Calculator")
-#     ##phase = st.radio("Choose the phase of your project", ("Production phase", "Construction Phase"))
+# # if st.button("Calculator", help="Explore Calculator", key="calculator_button", class_="button-style"):
+# #     st.title("Calculator")
+# #     phase = st.radio("Choose the phase of your project", ("Production phase", "Construction Phase"))
+# #     # Add functionality for the Calculator section...
 
-#     if phase == "Production phase":
-#         asphalt_type = st.radio("Choose the type of your asphalt", ("Standard Hot Mix Asphalt", "HMA with RAP"))
-#         # Add more selection choices for asphalt type and temperature...
-#         if asphalt_type == "Standard Hot Mix Asphalt":
-#             pass
-#             # Display result for Standard HMA based on selected temperature
-#             # Show the calculated CO2 emissions and details
-#             # Example: st.write(f"The total amount of CO2 for this asphalt with identifying 180 Celsius temperature in aggregate heating is...")
-#         else:
-#             pass# HMA with RAP
-#             # Display result for HMA with RAP similar to Standard HMA
-#             # Show the calculated CO2 emissions and details
-#     else:  # Construction Phase
-#         # Add functionality for the Construction Phase calculations
-#         pass
+
+# # ##else:  # Calculator section
+# #     ##st.title("Calculator")
+# #     ##phase = st.radio("Choose the phase of your project", ("Production phase", "Construction Phase"))
+
+# #     if phase == "Production phase":
+# #         asphalt_type = st.radio("Choose the type of your asphalt", ("Standard Hot Mix Asphalt", "HMA with RAP"))
+# #         # Add more selection choices for asphalt type and temperature...
+# #         if asphalt_type == "Standard Hot Mix Asphalt":
+# #             pass
+# #             # Display result for Standard HMA based on selected temperature
+# #             # Show the calculated CO2 emissions and details
+# #             # Example: st.write(f"The total amount of CO2 for this asphalt with identifying 180 Celsius temperature in aggregate heating is...")
+# #         else:
+# #             pass# HMA with RAP
+# #             # Display result for HMA with RAP similar to Standard HMA
+# #             # Show the calculated CO2 emissions and details
+# #     else:  # Construction Phase
+# #         # Add functionality for the Construction Phase calculations
+# #         pass
 
 import streamlit as st
 import pandas as pd
