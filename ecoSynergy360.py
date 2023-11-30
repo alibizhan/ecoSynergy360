96d31368f011a91dfe058a302900abbfa1901189
Ali Blau <alibizhan.samim@gmail.com>
2023-11-30 23:20:02 +0300

    update python file

--- ecoSynergy360.py
+++ ecoSynergy360.py
@@ -141,78 +141,78 @@ elif selected_section == "Carbon Footprint":
     # Add more content for Carbon Footprint section...
 
 else:  # Calculator section
-    st.title("Calculator")
-    phase = st.radio("Choose the phase of your project", ("Production phase", "Construction Phase"))
+    # st.title("Calculator")
+    # phase = st.radio("Choose the phase of your project", ("Production phase", "Construction Phase"))
 
-    if phase == "Production phase":
-        asphalt_type = st.radio("Choose the type of your asphalt", ("Standard Hot Mix Asphalt", "HMA with RAP"))
-        # Add more selection choices for asphalt type and temperature...
-        if asphalt_type == "Standard Hot Mix Asphalt":
-            pass
-            # Display result for Standard HMA based on selected temperature
-            # Show the calculated CO2 emissions and details
-            # Example: st.write(f"The total amount of CO2 for this asphalt with identifying 180 Celsius temperature in aggregate heating is...")
-        else:
-            pass# HMA with RAP
-            # Display result for HMA with RAP similar to Standard HMA
-            # Show the calculated CO2 emissions and details
-    else:  # Construction Phase
-        # Add functionality for the Construction Phase calculations
-        pass
-import streamlit as st
-import pandas as pd
+    # if phase == "Production phase":
+    #     asphalt_type = st.radio("Choose the type of your asphalt", ("Standard Hot Mix Asphalt", "HMA with RAP"))
+    #     # Add more selection choices for asphalt type and temperature...
+    #     if asphalt_type == "Standard Hot Mix Asphalt":
+    #         pass
+    #         # Display result for Standard HMA based on selected temperature
+    #         # Show the calculated CO2 emissions and details
+    #         # Example: st.write(f"The total amount of CO2 for this asphalt with identifying 180 Celsius temperature in aggregate heating is...")
+    #     else:
+    #         pass# HMA with RAP
+    #         # Display result for HMA with RAP similar to Standard HMA
+    #         # Show the calculated CO2 emissions and details
+    # else:  # Construction Phase
+    #     # Add functionality for the Construction Phase calculations
+    #     pass
+    import streamlit as st
+    import pandas as pd
 
-st.set_page_config(
-    page_title="ecoSynergy360",
-    page_icon="icons8-arrows-around-circle-64.png",
-    #layout="wide",
-)
-# Title and introductory text for the first page
-# st.title('WELCOME TO THE ECOSYNERGY360 PLATFORM ON SUSTAINABILITY ASSESSMENT')
-# st.markdown(
-#     """
-#     The EPSA is developed by Ecosynergy360 team...
-#     """
-# )
+    st.set_page_config(
+        page_title="ecoSynergy360",
+        page_icon="icons8-arrows-around-circle-64.png",
+        #layout="wide",
+    )
+    # Title and introductory text for the first page
+    # st.title('WELCOME TO THE ECOSYNERGY360 PLATFORM ON SUSTAINABILITY ASSESSMENT')
+    # st.markdown(
+    #     """
+    #     The EPSA is developed by Ecosynergy360 team...
+    #     """
+    # )
 
-# Sample data for the regression coefficients
-data = {
-    'Coefficients': [0.010080678, 0.088990197],
-    'Standard Error': [0.011865215, 4.86342E-05],
-    't Stat': [0.849599284, 1829.786459],
-    'P-value': [0.428140311, 1.79845E-18],
-    'Lower 95%': [-0.018952458, 0.088871194],
-    'Upper 95%': [0.039113815, 0.089109201],
-    'Lower 95,0%': [-0.018952458, 0.088871194],
-    'Upper 95,0%': [0.039113815, 0.089109201]
-}
+    # Sample data for the regression coefficients
+    data = {
+        'Coefficients': [0.010080678, 0.088990197],
+        'Standard Error': [0.011865215, 4.86342E-05],
+        't Stat': [0.849599284, 1829.786459],
+        'P-value': [0.428140311, 1.79845E-18],
+        'Lower 95%': [-0.018952458, 0.088871194],
+        'Upper 95%': [0.039113815, 0.089109201],
+        'Lower 95,0%': [-0.018952458, 0.088871194],
+        'Upper 95,0%': [0.039113815, 0.089109201]
+    }
 
-# Creating a DataFrame from the data
-df = pd.DataFrame(data, index=['Intercept', 'X Variable 1'])
+    # Creating a DataFrame from the data
+    df = pd.DataFrame(data, index=['Intercept', 'X Variable 1'])
 
-# Displaying the table using Streamlit
-st.table(df)
+    # Displaying the table using Streamlit
+    st.table(df)
 
 
-# Sample coefficients data
-data = {
-    "Coefficients": ["Intercept", "X Variable 1"],
-    "Coeff_Values": [0.010080678, 0.088990197],
-    "Std_Error": [0.011865215, 4.86342E-05]
-}
-coefficients = pd.DataFrame(data)
+    # Sample coefficients data
+    data = {
+        "Coefficients": ["Intercept", "X Variable 1"],
+        "Coeff_Values": [0.010080678, 0.088990197],
+        "Std_Error": [0.011865215, 4.86342E-05]
+    }
+    coefficients = pd.DataFrame(data)
 
-# Function to calculate CO2
-def calculate_CO2(input_val, coefficients):
-    intercept = coefficients.loc[coefficients['Coefficients'] == 'Intercept', 'Coeff_Values'].values[0]
-    x_variable_coeff = coefficients.loc[coefficients['Coefficients'] == 'X Variable 1', 'Coeff_Values'].values[0]
-    CO2 = x_variable_coeff * input_val + intercept
-    return CO2
+    # Function to calculate CO2
+    def calculate_CO2(input_val, coefficients):
+        intercept = coefficients.loc[coefficients['Coefficients'] == 'Intercept', 'Coeff_Values'].values[0]
+        x_variable_coeff = coefficients.loc[coefficients['Coefficients'] == 'X Variable 1', 'Coeff_Values'].values[0]
+        CO2 = x_variable_coeff * input_val + intercept
+        return CO2
 
-# Streamlit app
-st.title('CO2 Calculation')
+    # Streamlit app
+    st.title('CO2 Calculation')
 
-input_val = st.number_input("Enter the value for X Variable 1:", value=0.0)
-result = calculate_CO2(input_val, coefficients)
+    input_val = st.number_input("Enter the value for X Variable 1:", value=0.0)
+    result = calculate_CO2(input_val, coefficients)
 
-st.write(f"The calculated CO2 value is: {result}")
+    st.write(f"The calculated CO2 value is: {result}")
